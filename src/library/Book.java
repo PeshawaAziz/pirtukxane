6cbb6826a207cd63206ae005ebb166159a2bce60
Peshawa <modern3p2017@gmail.com>
2025-05-29 15:46:10 +0330

    feat: add waitlist logic to Book class

--- src/library/Book.java
+++ src/library/Book.java
@@ -1,40 +1,52 @@
 package library;
 
+import datastructures.queues.CustomQueue;
+
 public class Book {
     private String title;
     private String author;
     private String isbn;
     private boolean isAvailable;
-
-    // TODO: Define a data structure to hold members waiting for this book
+    private CustomQueue<Member> waitlist;
 
     public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.isAvailable = true;
-
-        // TODO: Initialize your data structure here
+        this.waitlist = new CustomQueue<>(10);
     }
 
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getIsbn() { return isbn; }
-    public boolean isAvailable() { return isAvailable; }
-    public void setAvailable(boolean available) { isAvailable = available; }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 
     public void addToWaitlist(Member member) {
-        // TODO
+        waitlist.add(member);
     }
 
     public Member getNextInWaitlist() {
-        // TODO
-        return null;
+        return waitlist.poll();
     }
 
     public boolean hasWaitlist() {
-        // TODO
-        return false;
+        return !waitlist.isEmpty();
     }
 
     @Override
